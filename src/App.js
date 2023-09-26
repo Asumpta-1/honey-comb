cf1f2092698baef8a09cc14bfda6a63d90f06403
asumpta <sumgathoni@gmail.com>
2023-09-26 12:44:57 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TodoList from "./TodoList";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList className="todo-list" />
+    </div>
+  );
+}
+
+export default App;
